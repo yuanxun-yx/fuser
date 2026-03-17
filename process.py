76c95e63f65c4789a826abe404943343df322852
Xun Yuan <yuanxun2000@outlook.com>
2026-03-17 03:29:16 -0400

    add hrf

--- process.py
+++ process.py
@@ -4,6 +4,8 @@ import numpy as np
 from numpy.linalg import inv
 from scipy.linalg import lstsq
 from scipy.ndimage import affine_transform, label, binary_fill_holes, binary_closing
+from scipy.signal import detrend, convolve
+from nilearn.glm.first_level import spm_hrf, glover_hrf
 
 from dataset import Dataset
 
@@ -48,7 +50,7 @@ def bincount_axes(
     x = np.moveaxis(x, axis, dest)
 
     batch_shape = x.shape[:-len(axis)]
-    reduce_size = np.prod(x.shape[-len(axis):]).item()
+    reduce_size = np.prod(x.shape[-len(axis):])
 
     x = x.reshape(-1, reduce_size)
 
@@ -98,12 +100,24 @@ def process_fus(
         time += fus_delay_s
 
         epochs = session.epochs
-        # shape: event + 1, scan repeat, pose
-        event_mask = np.empty((len(epochs) + 1, *data.shape[:2]), dtype=bool)
-        # intercept
-        event_mask[-1] = 1
+        # shape: event, scan repeat, pose
+        event_mask = np.empty((len(epochs), *data.shape[:2]), dtype=bool)
         for i, v in enumerate(session.epochs.values()):
             event_mask[i] = ((time[..., None] >= v[:, 0]) & (time[..., None] <= v[:, 1])).any(axis=-1)
+        # haemodynamic response
+        diff = np.diff(time, axis=0)
+        dt = diff.mean()
+        if not np.allclose(dt, diff):
+            raise ValueError(f"dt not identical for session {fus_scan.metadata.file_id.hex()}")
+        hrf = glover_hrf(dt)
+        kernel = np.array([.25, .5, .25])
+        # shape: event + 1, scan repeat, pose
+        design = np.empty((event_mask.shape[0] + 1, *event_mask.shape[1:]), dtype=hrf.dtype)
+        # intercept
+        design[-1] = 1
+        for i in range(event_mask.shape[0]):
+            for j in range(event_mask.shape[-1]):
+                design[i, :, j] = convolve(event_mask[i, :, j], kernel, mode='same')
 
         # each pose + 3d correspond to one voxel in fixed space, not 3d alone
         # because probe is moving, think as 4d coordinate
@@ -124,15 +138,16 @@ def process_fus(
                 s[:, k, :] = binary_closing(binary_fill_holes(body_mask[:, k, :]))
 
         # do not perform log here to preserve linearity
+        data = detrend(data, axis=0)
         # z-score: within scan repeats
-        mean = data.mean(axis=0, keepdims=True)
+        # mean is done by detrend
         std = data.std(axis=0, keepdims=True)
         # avoid 0/0 = NaN
-        data = np.where(std > 0, (data - mean) / std, 0)
+        data = np.where(std > 0, data / std, 0)
 
         # GLM: to use event to explain fUS
         # shape: pose, scan repeat, event + 1
-        x = event_mask.T
+        x = design.T
         # shape: pose, scan repeat, voxels
         y = data.reshape(*data.shape[:2], -1).swapaxes(0, 1)
         # shape: pose, event + 1, voxels
