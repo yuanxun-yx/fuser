f02217ac989e19e9f63be19c0f40b833183fcecc
Xun Yuan <yuanxun2000@outlook.com>
2026-03-22 23:17:17 -0400

    refactor: move bincount_axes to utils.py

--- process.py
+++ process.py
@@ -11,51 +11,7 @@ from ontology import RoiIds
 from registration import transform
 from event import get_event_df, EVENT_NAME, NON_EVENT_NAME
 from progress import ProgressReporter
-
-
-def bincount_axes(
-    x: np.ndarray,
-    /,
-    axis: int | tuple[int, ...] | None = None,
-    weights: np.ndarray = None,
-) -> np.ndarray:
-    """
-    apply numpy.bincount on given axes of x
-    """
-    if weights is not None:
-        weights = np.broadcast_to(weights, x.shape)
-
-    if axis is None:
-        axis = tuple(range(x.ndim))
-    if isinstance(axis, int):
-        axis = (axis,)
-
-    # move flattened axes to the end
-    dest = tuple(range(-len(axis), 0))
-    x = np.moveaxis(x, axis, dest)
-
-    batch_shape = x.shape[: -len(axis)]
-    reduce_size = np.prod(x.shape[-len(axis) :])
-
-    x = x.reshape(-1, reduce_size)
-
-    k = x.max() + 1
-    b = x.shape[0]
-
-    offset = np.arange(b)[:, None] * k
-    x = (x + offset).ravel()
-
-    # apply same flatten to weights
-    if weights is not None:
-        weights = np.moveaxis(weights, axis, dest)
-        weights = weights.reshape(-1, reduce_size)
-        weights = weights.ravel()
-
-    counts = np.bincount(x, weights=weights, minlength=b * k)
-
-    counts = counts.reshape(*batch_shape, k)
-
-    return counts
+from utils import bincount_axes
 
 
 def process_fus(
