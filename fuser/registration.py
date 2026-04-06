0e14fa215db78b47bcd54ac9c5c0876be3fc5e9e
Xun Yuan <yuanxun2000@outlook.com>
2026-04-06 17:37:01 -0400

    fix: typo in comment

--- fuser/registration.py
+++ fuser/registration.py
@@ -6,7 +6,7 @@ from skimage.registration import phase_cross_correlation
 # in um
 BRAIN_ORIGIN_CCFv3_COORD_POST_INF_RIGHT = (5500, 5300, 6100)
 
-# Iconeous "brain" coordinate
+# Iconeus "brain" coordinate
 # direction: left, anterior, superior (dorsal)
 # origin: approx 4mm below Bregma
 # CCFv3 axis order: posterior, inferior, right
@@ -18,7 +18,7 @@ BRAIN_TO_ANNOTATION[0, 1] = -1
 BRAIN_TO_ANNOTATION[1, 2] = -1
 BRAIN_TO_ANNOTATION[2, 0] = -1
 BRAIN_TO_ANNOTATION[:3, 3] = BRAIN_ORIGIN_CCFv3_COORD_POST_INF_RIGHT
-# magic number from Iconeous: 4mm (4000 um)
+# magic number from Iconeus: 4mm (4000 um)
 BRAIN_TO_ANNOTATION[:3, :3] *= 4e3
 
 
